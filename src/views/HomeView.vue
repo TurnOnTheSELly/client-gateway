b09c3cb38f00c8cfe236341b8c09b943e2f2e65b
Joop Stark <stark.joop@gmail.com>
2024-04-16 14:49:15 -0600

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import DataTable from '../components/DataTable.vue'
+</script>
+
+<template>
+  <main>
+    <DataTable />
+  </main>
+</template>
