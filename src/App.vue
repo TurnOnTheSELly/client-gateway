fb2f8792a9f60c907ec1f405ae8717b7b26d912f
Joop Stark <stark.joop@gmail.com>
2024-05-28 10:12:38 -0600

    Still setting up login

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink> |
+        <RouterLink to="/about">About</RouterLink> 
+        <RouterLink to="/login">Login</RouterLink>
         <!-- <RouterLink to="/games_teacher">Games (teacher)</RouterLink> -->
       </nav>
     </div>
