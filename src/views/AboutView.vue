c7399f7e9c1f780adf7aab48ffe32be1f89e8ad5
joop <stark.joop@gmail.com>
2024-09-25 20:57:56 -0600

    about individuals and technologies added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <script setup>
 const biographies = [
-  ["Joop Stark", "Web Developer and System Administrator", "I am an Elementary School Teacher turned Web Developer. Though in the tech industry, I am passionate about getting our children outside and experiencing the world. That is why I am thrilled to be writing code and configuring Docker and AWS for a company whose mission is to use screens more efficiently.", "joop"]
+  {name: "Joop Stark",
+  position: "Web Developer and System Administrator",
+  description: "I am an Elementary School Teacher turned Web Developer. Though in the tech industry, I am passionate about getting our children outside and experiencing the world. That is why I am thrilled to be writing code and configuring Docker and AWS for a company whose mission is to use screens more efficiently.", 
+  profilePic:"joop"}
 ]
 const test = "joop"
 
@@ -18,15 +21,27 @@ const test = "joop"
   <div class="about-individuals">
     <div v-for="bio in biographies">
       <div class="individual-bio">
-        <img alt="profile" :src="`/src/assets/images/profiles/${bio[3]}.png`" /> 
-        <p>{{ bio[0] }}</p>
-        <p>{{ bio[1] }}</p>
-        <p>{{ bio[2] }}</p>
+        <img alt="profile" :src="`/src/assets/images/profiles/${bio.profilePic}.png`" /> 
+        <h2>{{ bio.name }}</h2>
+        <h3>{{ bio.position }}</h3>
+        <p>{{ bio.description }}</p>
       </div>
     </div>
-
-
   </div>
+
+  <div class="about-programming">
+    <!-- <p>Website has a static front-end written in Vue.js and stored in an AWS S3 bucket, with a dockerized Ruby on Rails api running in an EC2 instance. TLS is managed with Cloudfront. </p> -->
+    <h3>Technologies</h3>
+    <p>AWS: S3, ECR, EC2(with Ubuntu), RDS, Cloudfront, Route 53</p>
+    <p>Backend: Ruby on Rails, Docker, Raw SQL, Postgres, Bash</p>
+    <p>Frontend: Vue.js</p>
+
+    <h3>Databases</h3>
+    <p>Internet Movie Database</p>
+    <p>Just Watch</p>
+  </div>
+
+
 </template>
 
 <style>
@@ -39,6 +54,44 @@ const test = "joop"
     .warning {
       padding: 20px;
       color: rgb(87, 87, 2);
+    }
+  }
+
+  .about-individuals {
+   div {
+    div.individual-bio{
+      background-color: beige;
+      border-radius: 20px;
+      display: inline-block;
+      width: 300px;
+      /* margin: 0 auto; */
+
+      img {
+        margin-top: 20px;
+        margin-left: 50px;
+      }
+
+      h2 {
+        margin-left: 10px;
+      }
+      h3 {
+        color: black;
+        margin-left: 10px;
+        margin-right: 10px;
+      }
+      p {
+        margin: 10px 10px 15px 10px;
+      }
+    }
+   }
+   margin-bottom: 50px;
+  }
+
+  .about-programming {
+    margin-bottom: 50px;
+    h3 {
+      color: black;
+      text-decoration: underline;
     }
   }
 }
