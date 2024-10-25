3bb43dd99a50f0e0f0f234601c9f561eb543c40e
joop <stark.joop@gmail.com>
2024-10-25 14:00:52 -0600

    bug fix to lowercase

--- src/components/snowman/Popup.vue
+++ src/components/snowman/Popup.vue
@@ -24,7 +24,7 @@
 
   const reset = () => { emit('reset'); };
   const supply = () => { 
-    emit('supply', phrase.value); 
+    emit('supply', phrase.value.toLowerCase()); 
     form.value = false
   };
 
